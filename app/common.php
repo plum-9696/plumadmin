f0ce911db92aab8155a1fb35928dfc84668eaa78
plum <plumcloud_coder@163.com>
2021-11-29 14:59:25 +0800

    增加migrate,获取当前登录ID公共函数

--- app/common.php
+++ app/common.php
@@ -1,2 +1,14 @@
 <?php
 // 应用公共文件
+
+
+/**
+ * 获取当前模块登录验证的用户ID
+ * @author Plum
+ * @email liujunyi_coder@163.com
+ * @time 2021年11月29日 14:17
+ */
+function get_user_id()
+{
+    return \plum\lib\Token::auth(app('http')->getName(), false);
+}
